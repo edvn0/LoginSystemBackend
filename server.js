b811c08014d9a3387d47a9db4d8f0b274edb319f
Edwin Carlsson <edwin98carlsson@gmail.com>
2019-09-26 11:35:40 +0200

    fixes

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const prod = process.env.NODE_ENV === "production";
+const origin = {
+    origin: prod ? "https://login-app-angular.herokuapp.com/" : "*"
+};
+
 const app = express();
 
-app.use(cors());
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
-if (process.env.NODE_ENV !== "production") {
+if (prod) {
     app.use(morgan('combined'));
+    app.use(cors(origin));
 }
 
 const apiRoute = require('./routes/apiRoute');
