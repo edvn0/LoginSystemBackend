d8f023328880937bd3f915018989abda56d07ecd
Edwin Carlsson <edwin98carlsson@gmail.com>
2019-09-26 11:23:48 +0200

    fixes

--- database/db_config.js
+++ database/db_config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDbtUY7gCXv_eL9s0mXLhWPrCLxEKONcaA",
-  authDomain: "loginappangular-14874.firebaseapp.com",
-  databaseURL: "https://loginappangular-14874.firebaseio.com",
-  projectId: "loginappangular-14874",
-  storageBucket: "loginappangular-14874.firebaseapp.com",
-  messagingSenderId: "758581710472",
-  appId: "1:758581710472:web:b7a6516d07c17f7f",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.app,
 };
 
 module.exports = firebaseConfig;
